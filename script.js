b02497fb6de83456bd62c7ec0cb0717bd14deb9f
Ricky Santiago <145139705+fizzle1620@users.noreply.github.com>
2025-01-09 16:21:01 +0800

    Practice js

---
+++ script.js
@@ -0,0 +1,33 @@
+let day = 2;
+
+
+switch(day){
+    
+    case 1:
+        console.log(`it's monday`);
+        break;
+        case 2:
+        console.log(`it's tuesday`);
+        break;
+        case 3:
+        console.log(`it's wednesday`);
+        break;
+        case 4:
+        console.log(`it's thursday`);
+        break;
+        case 5:
+        console.log(`it's friday`);
+        break;
+        case 6:
+        console.log(`it's saturday`);
+        break;
+        case 7:
+        console.log(`it's sunday`);
+        break;
+        default:
+            console.log(`${day} is not a day`)
+
+                
+
+
+}
